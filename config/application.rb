025a0293d8e16b476ed666527b164f0087374d87
Kevin <kevinnoll90@gmail.com>
2014-05-27 11:35:39 +0200

    disabled parts of the assets pipeline
    needed so every controller can load its own stylesheets. otherwise every
    page loads every css file in the assets folder.

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,8 @@ module SampleApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    # render all individual css files. 
+    # needed that every controller can have his own stylesheet.
+    config.assets.precompile << "*.css"
   end
 end
