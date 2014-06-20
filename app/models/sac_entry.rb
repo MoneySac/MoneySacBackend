eb4a0df2c6bebfed4240a86f7d70d37ce6e5bd7a
Noll <kevin.noll@sap.com>
2014-06-20 14:01:44 +0200

    fixed tests for sac entries

--- app/models/sac_entry.rb
+++ app/models/sac_entry.rb
@@ -1,3 +1,4 @@
 class SacEntry < ActiveRecord::Base
+	validates :description, :amount, :category_id, :date, :type_id, :recurring, presence: true
 	belongs_to :category
 end
--- test/controllers/sac_entries_controller_test.rb
+++ test/controllers/sac_entries_controller_test.rb
@@ -3,9 +3,17 @@ include Devise::TestHelpers
 
 class SacEntriesControllerTest < ActionController::TestCase
   setup do
-    @sac_entry = sac_entries(:one)
     @user = users(:one)
     sign_in @user
+    @sac_entry = sac_entries(:one)
+    @update = {
+      description: 'testSac', 
+      amount: 22,
+      category_id: 1,
+      date: Time.now,
+      type_id: 2,
+      recurring: true
+    }
   end
 
   test "should get index" do
@@ -21,7 +29,7 @@ class SacEntriesControllerTest < ActionController::TestCase
 
   test "should create sac_entry" do
     assert_difference('SacEntry.count') do
-      post :create, sac_entry: { amount: @sac_entry.amount, category_id: @sac_entry.category_id, date: @sac_entry.date, description: @sac_entry.description, recurring: @sac_entry.recurring, type_id: @sac_entry.type_id }
+      post :create, sac_entry: @update
     end
 
     assert_redirected_to sac_entry_path(assigns(:sac_entry))
@@ -38,7 +46,7 @@ class SacEntriesControllerTest < ActionController::TestCase
   end
 
   test "should update sac_entry" do
-    patch :update, id: @sac_entry, sac_entry: { amount: @sac_entry.amount, category_id: @sac_entry.category_id, date: @sac_entry.date, description: @sac_entry.description, id: @sac_entry.id, recurring: @sac_entry.recurring, type_id: @sac_entry.type_id }
+    patch :update, id: @sac_entry, sac_entry: @update
     assert_redirected_to sac_entry_path(assigns(:sac_entry))
   end
 
