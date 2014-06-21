a675e31b3369429bd6a6a77601ec0d03df0f6784
Kevin <kevinnoll90@gmail.com>
2014-06-21 17:38:29 +0200

    added unittest for category, validation for type_id

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
 	validates :name, :type_id, presence: true
+	validates :type_id, numericality: {greater_than_or_equal_to:0}
 	has_many :sac_entries
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "category attributes must not be empty" do
+    category = Category.new
+    assert category.invalid?
+    assert category.errors[:name].any?
+    assert category.errors[:type_id].any?
+  end
+
+  test "category type_id must be greater than or equal to zero" do
+  	category = Category.new(name: 'testCat')
+  	
+  	category.type_id = -1
+  	assert category.invalid?
+  	assert_equal ["must be greater than or equal to 0"], category.errors[:type_id]
+
+  	category.type_id = 0
+  	assert category.valid?
+  end
 end
