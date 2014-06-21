53ce48a7cb4b6b35e6cea086fdfc3e7d75b8d6ab
Kevin <kevinnoll90@gmail.com>
2014-06-21 18:35:17 +0200

    sacentry unittests

--- test/models/sac_entry_test.rb
+++ test/models/sac_entry_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class SacEntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "sacentry attributes must not be empty" do
+    sacentry = SacEntry.new
+    assert sacentry.invalid?
+    assert sacentry.errors[:description].any?
+    assert sacentry.errors[:amount].any?
+    assert sacentry.errors[:category_id].any?
+    assert sacentry.errors[:date].any?
+    assert sacentry.errors[:type_id].any?
+  end
 end
