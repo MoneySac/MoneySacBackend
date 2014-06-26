06aa6737ab7dfa33a6dfd8f7a7c62837dd2740a9
Noll <kevin.noll@sap.com>
2014-06-26 15:51:59 +0200

    added owner to categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,5 +3,5 @@ class Category < ActiveRecord::Base
   validates :type_id, numericality: {greater_than_or_equal_to:0}
   has_many :sac_entries
   has_and_belongs_to_many :users
-
+  belongs_to :user
 end
