c0eaa0a89548ae3ce16b5bd28aa66704058dfefd
Noll <kevin.noll@sap.com>
2014-06-26 15:05:28 +0200

    cleanup, now edit view needed

--- app/controllers/category_user_controller.rb
+++ app/controllers/category_user_controller.rb
@@ -8,6 +8,7 @@ class CategoryUserController < ApplicationController
   	unless Category.find(params[:category_id]).users.exists?(current_user)
   		Category.find(params[:category_id]).users << current_user
   	end
+  	redirect_to category_user_index_path
   end
 
   def edit
