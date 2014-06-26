69fe02d7e659c67399a923fd39362a937834403c
Noll <kevin.noll@sap.com>
2014-06-26 14:45:50 +0200

    users can now add themselves to categories

--- app/controllers/category_user_controller.rb
+++ app/controllers/category_user_controller.rb
@@ -4,6 +4,10 @@ class CategoryUserController < ApplicationController
   end
 
   def new
+  	# if the user is not already added, add him to the category
+  	unless Category.find(params[:category_id]).users.exists?(current_user)
+  		Category.find(params[:category_id]).users << current_user
+  	end
   end
 
   def edit
