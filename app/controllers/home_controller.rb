33b0e203b43cf456c746fe5813502e57af4da76a
Kevin <kevinnoll90@gmail.com>
2014-06-29 16:06:25 +0200

    10 most popular categories

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,12 @@
 class HomeController < ApplicationController
   def overview
-  	@recent_sacentries = SacEntry.where("DATE(created_at) > DATE(?) AND user_id = ?", 1.week.ago, current_user.id)
-  	@newest_categories = Category.limit(10).order('id desc')
+    @recent_sacentries = SacEntry.where("DATE(created_at) > DATE(?) AND user_id = ?", 1.week.ago, current_user.id)
+    @newest_categories = Category.limit(10).order('id desc')
 
-  	@cat_hash = Hash.new
-  	Category.all.each do |c|
-  		@cat_hash[c] = c.users.count()
-  	end
-  	
+    @cat_hash = Hash.new
+    Category.all.each do |c|
+      @cat_hash[c.name] = c.users.count()
+    end
+
   end
 end
