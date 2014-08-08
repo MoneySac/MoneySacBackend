c1ebf4c5351dcb7a02f7ea4b6641f6b1bbf42901
Kevin Noll <kevinnoll90@gmail.com>
2014-08-08 17:39:29 +0200

    setup Mac Pro

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
+
   def overview
     @recent_sacentries = SacEntry.where("DATE(created_at) > DATE(?) AND user_id = ?", 1.week.ago, current_user.id)
     @newest_categories = Category.limit(10).order('id desc')
-
     @cat_hash = Hash.new
     Category.all.each do |c|
       @cat_hash[c.name] = c.users.count()
